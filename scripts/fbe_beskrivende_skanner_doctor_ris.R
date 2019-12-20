730ed4c83bb53b6d6e40105cc92b3a46e0d11149
Mirza Hasanbasic <lekroya@gmail.com>
2019-12-20 15:18:31 +0100

    besk. skanner dr mod

--- scripts/fbe_beskrivende_skanner_doctor_ris.R
+++ scripts/fbe_beskrivende_skanner_doctor_ris.R
@@ -33,20 +33,57 @@ beskrivende_doctor <- read.csv2(
 )
 
 
-tmp <- grep("Frank", beskrivende_doctor$Full.name)
 
-print(length(tmp))
+# sort( unique(beskrivende_doctor$Full.name))
+
+unique(beskrivende_doctor$Full.name)[grep("Annette", unique(beskrivende_doctor$Full.name))]
+
+beskrivende_doctor$sub_code <- substr(beskrivende_doctor$Billing.code, 1, 3)
+
+#choosing only UXM codes (MR)
+
+beskrivende_doctor_mr <- beskrivende_doctor[beskrivende_doctor$sub_code %in% "UXM", ]
+
+mr_forskere <- tibble(
+    Full.name = c("Jensen, Frank Krieger", "Buskov, Laura,", "Jensen, Annette Bøjer,",
+              "Magnussen, Erland,", "Madsen, Camilla,", "Davtyan, Marine,", 
+              "Mariana Kristensen,", "Joshi, Deepak,"),
+    codes = c(rep(334, 3), rep(340, 5))
+)
+
+mr_forskere <- as.data.frame(mr_forskere)
+
+beskrivende_doctor_mr <- merge(x = beskrivende_doctor_mr, y = mr_forskere, 
+                               by = "Full.name", all.x = TRUE)
 
 
-name_lookup <- "Jensen, Frank Krieger"
-
-bd_frank <- beskrivende_doctor[beskrivende_doctor$Full.name %in% name_lookup, ]
 
 
-bd_frank$sub_code <- substr(bd_frank$Billing.code, 1, 3)
+
+fordeling <- beskrivende_doctor_mr %>%
+    count(codes, Full.name, Billing.code) %>%  
+    mutate(andel_i_procent = round(n/sum(n)*100, 2)) 
 
 
-fordeling <- bd_frank %>% 
-    count(sub_code, sort=TRUE) %>%  
-    mutate(andel = round(n/sum(n)*100, 2)) 
+fordeling[is.na(fordeling$codes), c("codes")] <- 0
 
+
+pr_grp <- aggregate(fordeling$n, by=list(codes=fordeling$codes), FUN=sum)
+
+fordeling <- merge(x = fordeling, y = pr_grp, by = "codes", all.x = TRUE)
+
+
+fordeling$andel_i_grp <- round(fordeling$n / fordeling$x * 100, 2)
+
+fordeling <- fordeling[, !(names(fordeling) %in% "x")]
+
+
+
+save_file_name <- paste0(save_data_loc, "oversigt_beskrivetider.csv")
+write.csv(as.matrix(fordeling), save_file_name, row.names = TRUE)
+
+
+tmp <- fordeling[fordeling$codes %in% 0, c("Full.name", "Billing.code")]
+
+save_file_name <- paste0(save_data_loc, "mr_beskrivelser_alt_andet_end_334_340.csv")
+write.csv(as.matrix(tmp), save_file_name, row.names = TRUE)
